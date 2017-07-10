44d82ba96816368161afdec7aa47bbf4fd149270
Duncan Chiang <duncan.c@inwinstack.com>
2017-07-10 18:15:37 +0800

    [Bugfix] Add check files_externel_moe app for user to add navigation

--- lib/navigation.php
+++ lib/navigation.php
@@ -2,24 +2,24 @@
 
 /**
  * ownCloud - Activity App
- *
- * @author Joas Schilling
- * @copyright 2014 Joas Schilling nickvergessen@owncloud.com
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+*
+* @author Joas Schilling
+* @copyright 2014 Joas Schilling nickvergessen@owncloud.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 
 namespace OCA\Activity;
 
@@ -33,172 +33,174 @@ use \OCP\Template;
  * @package OCA\Activity
  */
 class Navigation {
-	/** @var \OC_L10N */
-	protected $l;
+    /** @var \OC_L10N */
+    protected $l;
 
-	/** @var \OCP\Activity\IManager */
-	protected $activityManager;
+    /** @var \OCP\Activity\IManager */
+    protected $activityManager;
 
-	/** @var \OCP\IURLGenerator */
-	protected $URLGenerator;
+    /** @var \OCP\IURLGenerator */
+    protected $URLGenerator;
 
-	/** @var UserSettings */
-	protected $userSettings;
+    /** @var UserSettings */
+    protected $userSettings;
 
-	/** @var string */
-	protected $active;
+    /** @var string */
+    protected $active;
 
-	/** @var string */
-	protected $user;
+    /** @var string */
+    protected $user;
 
-	/** @var string */
-	protected $rssLink;
+    /** @var string */
+    protected $rssLink;
 
-	/**
-	 * Construct
-	 *
-	 * @param \OC_L10N $l
-	 * @param \OCP\Activity\IManager $manager
-	 * @param \OCP\IURLGenerator $URLGenerator
-	 * @param UserSettings $userSettings
-	 * @param string $user
-	 * @param string $rssToken
-	 * @param null|string $active Navigation entry that should be marked as active
-	 */
-	public function __construct(\OC_L10N $l,
-								IManager $manager,
-								IURLGenerator $URLGenerator,
-								UserSettings $userSettings,
-								$user,
-								$rssToken,
-								$active = 'all') {
-		$this->l = $l;
-		$this->activityManager = $manager;
-		$this->URLGenerator = $URLGenerator;
-		$this->userSettings = $userSettings;
-		$this->user = $user;
-		$this->active = $active;
+    /**
+     * Construct
+     *
+     * @param \OC_L10N $l
+     * @param \OCP\Activity\IManager $manager
+     * @param \OCP\IURLGenerator $URLGenerator
+     * @param UserSettings $userSettings
+     * @param string $user
+     * @param string $rssToken
+     * @param null|string $active Navigation entry that should be marked as active
+     */
+    public function __construct(\OC_L10N $l,
+            IManager $manager,
+            IURLGenerator $URLGenerator,
+            UserSettings $userSettings,
+            $user,
+            $rssToken,
+            $active = 'all') {
+                $this->l = $l;
+                $this->activityManager = $manager;
+                $this->URLGenerator = $URLGenerator;
+                $this->userSettings = $userSettings;
+                $this->user = $user;
+                $this->active = $active;
 
-		if ($rssToken) {
-			$this->rssLink = $this->URLGenerator->getAbsoluteURL(
-				$this->URLGenerator->linkToRoute('activity.Feed.show', array('token' => $rssToken))
-			);
-		} else {
-			$this->rssLink = '';
-		}
-	}
+                if ($rssToken) {
+                    $this->rssLink = $this->URLGenerator->getAbsoluteURL(
+                            $this->URLGenerator->linkToRoute('activity.Feed.show', array('token' => $rssToken))
+                            );
+                } else {
+                    $this->rssLink = '';
+                }
+    }
 
-	/**
-	 * Get the users we want to send an email to
-	 *
-	 * @param null|string $forceActive Navigation entry that should be marked as active
-	 * @return \OCP\Template
-	 */
-	public function getTemplate($forceActive = null) {
-		$active = $forceActive ?: $this->active;
+    /**
+     * Get the users we want to send an email to
+     *
+     * @param null|string $forceActive Navigation entry that should be marked as active
+     * @return \OCP\Template
+     */
+    public function getTemplate($forceActive = null) {
+        $active = $forceActive ?: $this->active;
 
-		$template = new Template('activity', 'stream.app.navigation', '');
-		$entries = $this->getLinkList();
+        $template = new Template('activity', 'stream.app.navigation', '');
+        $entries = $this->getLinkList();
 
-		if (sizeof($entries['apps']) === 1) {
-			// If there is only the files app, we simply do not show it,
-			// as it is the same as the 'all' filter.
-			$entries['apps'] = array();
-		}
+        if (sizeof($entries['apps']) === 1) {
+            // If there is only the files app, we simply do not show it,
+            // as it is the same as the 'all' filter.
+            $entries['apps'] = array();
+        }
 
-		$template->assign('activeNavigation', $active);
-		$template->assign('navigations', $entries);
-		$template->assign('rssLink', $this->rssLink);
+        $template->assign('activeNavigation', $active);
+        $template->assign('navigations', $entries);
+        $template->assign('rssLink', $this->rssLink);
 
-		return $template;
-	}
+        return $template;
+    }
 
-	/**
-	 * Get all items for the users we want to send an email to
-	 *
-	 * @return array Notification data (user => array of rows from the table)
-	 */
-	public function getLinkList() {
-		if(\OC::$server->getSystemConfig()->getValue("theme", NUll) == "MOE") {
-			$topEntries = [
-				[
-					'icon' => 'files',
-					'id' => 'fileList',
-					'name' => (string) $this->l->t('All files'),
-				],
-				[
-					'icon' => 'favorites',
-					'id' => 'fileList-favorites',
-					'name' => (string) $this->l->t('Favorites'),
-					
-				],
-				[
-					'icon' => 'sharingin',
-					'id' => 'fileList-sharingin',
-					'name' => (string) $this->l->t('Shared with you'),
-				],
-				[
-					'icon' => 'sharingout',
-					'id' => 'fileList-sharingout',
-					'name' => (string) $this->l->t('Shared with others'),
-				],
-				[
-					'icon' => 'sharinglinks',
-					'id' => 'fileList-sharinglinks',
-					'name' => 'Shared with links',
-				],
-				[
-					'icon' => 'extstoragemounts',
-					'id' => 'fileList-extstoragemounts',
-					'name' => (string) $this->l->t('External sotrage'),
-				],
-				[
-					'icon' => 'activity',
-					'id' => 'all',
-					'name' => (string) $this->l->t('All Activities'),
-					'url' => $this->URLGenerator->linkToRoute('activity.Activities.showList')
-				],
-				[
-					'icon' => 'trashbin',
-					'id' => 'fileList-trashbin',
-					'name' => (string) $this->l->t('Deleted files'),
-				]
-			];
-		}
+    /**
+     * Get all items for the users we want to send an email to
+     *
+     * @return array Notification data (user => array of rows from the table)
+     */
+    public function getLinkList() {
+        if(\OC::$server->getSystemConfig()->getValue("theme", NUll) == "MOE") {
+            $topEntries = [
+                    [
+                            'icon' => 'files',
+                            'id' => 'fileList',
+                            'name' => (string) $this->l->t('All files'),
+                    ],
+                    [
+                            'icon' => 'favorites',
+                            'id' => 'fileList-favorites',
+                            'name' => (string) $this->l->t('Favorites'),
+                            	
+                    ],
+                    [
+                            'icon' => 'sharingin',
+                            'id' => 'fileList-sharingin',
+                            'name' => (string) $this->l->t('Shared with you'),
+                    ],
+                    [
+                            'icon' => 'sharingout',
+                            'id' => 'fileList-sharingout',
+                            'name' => (string) $this->l->t('Shared with others'),
+                    ],
+                    [
+                            'icon' => 'sharinglinks',
+                            'id' => 'fileList-sharinglinks',
+                            'name' => 'Shared with links',
+                    ],
+                    [
+                            'icon' => 'activity',
+                            'id' => 'all',
+                            'name' => (string) $this->l->t('All Activities'),
+                            'url' => $this->URLGenerator->linkToRoute('activity.Activities.showList')
+                    ],
+                    [
+                            'icon' => 'trashbin',
+                            'id' => 'fileList-trashbin',
+                            'name' => (string) $this->l->t('Deleted files'),
+                    ]
+            ];
+            if(\OC::$server->getAppManager()->isEnabledForUser('files_external_moe')){
+                array_splice($topEntries,4,0,[
+                            'icon' => 'extstoragemounts',
+                            'id' => 'fileList-extstoragemounts',
+                            'name' => (string) $this->l->t('External sotrage'),
+                    ]);
+            }
+        }
 
-		else {
-			$topEntries = [
-				[
-					'icon' => 'activity',
-					'id' => 'all',
-					'name' => (string) $this->l->t('All Activities'),
-					'url' => $this->URLGenerator->linkToRoute('activity.Activities.showList')
-				]
-			];
-		}
+        else {
+            $topEntries = [
+                    [
+                            'icon' => 'activity',
+                            'id' => 'all',
+                            'name' => (string) $this->l->t('All Activities'),
+                            'url' => $this->URLGenerator->linkToRoute('activity.Activities.showList')
+                    ]
+            ];
+        }
 
 
-		if ($this->user && $this->userSettings->getUserSetting($this->user, 'setting', 'self') && 
-			\OC::$server->getSystemConfig()->getValue("theme", NUll) != "MOE") {
-			$topEntries[] = [
-				'id' => 'self',
-				'name' => (string) $this->l->t('Activities by you'),
-				'url' => $this->URLGenerator->linkToRoute('activity.Activities.showList', array('filter' => 'self')),
-			];
-			$topEntries[] = [
-				'id' => 'by',
-				'name' => (string) $this->l->t('Activities by others'),
-				'url' => $this->URLGenerator->linkToRoute('activity.Activities.showList', array('filter' => 'by')),
-			];
+        if ($this->user && $this->userSettings->getUserSetting($this->user, 'setting', 'self') &&
+                \OC::$server->getSystemConfig()->getValue("theme", NUll) != "MOE") {
+                    $topEntries[] = [
+                            'id' => 'self',
+                            'name' => (string) $this->l->t('Activities by you'),
+                            'url' => $this->URLGenerator->linkToRoute('activity.Activities.showList', array('filter' => 'self')),
+                    ];
+                    $topEntries[] = [
+                            'id' => 'by',
+                            'name' => (string) $this->l->t('Activities by others'),
+                            'url' => $this->URLGenerator->linkToRoute('activity.Activities.showList', array('filter' => 'by')),
+                    ];
 
-		}
+                }
 
-		$additionalEntries = $this->activityManager->getNavigation();
-		$topEntries = array_merge($topEntries, $additionalEntries['top']);
+                $additionalEntries = $this->activityManager->getNavigation();
+                $topEntries = array_merge($topEntries, $additionalEntries['top']);
 
-		return array(
-			'top'		=> $topEntries,
-			'apps'		=> $additionalEntries['apps'],
-		);
-	}
+                return array(
+                        'top'		=> $topEntries,
+                        'apps'		=> $additionalEntries['apps'],
+                );
+    }
 }
